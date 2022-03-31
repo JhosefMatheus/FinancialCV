11e2d34839ccedc953315ac92be8f55295f6b6e9
Douglas124 <douglas.nunes.d.santos@gmail.com>
2022-03-30 21:41:06 -0300

    foi adicionado na classe a função de adiquirir os frames

--- VideoHandler.py
+++ VideoHandler.py
@@ -1,4 +1,29 @@
+import cv2
+import numpy as np
+
 class VideoHandler:
 
     def __init__(self, video_path):
         self.__video_path = video_path
+
+    def get_frames(self):
+        c = 1
+        video = cv2.VideoCapture(self.__video_path)
+
+        ret, frames = video.read()
+        while ret:
+            ret, frame = video.read()
+            try:
+                frames = np.append(frames, frame, axis=0)
+                c +=1
+
+            except Exception as e:
+                pass
+        frames = np.array(np.split(frames, c))
+        frames = np.delete(frames, 0,0)
+
+        return frames  
+
+
+
+
---
+++ main.py
@@ -0,0 +1,1 @@
+from VideoHandler import VideoHandler
